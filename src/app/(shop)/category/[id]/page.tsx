6318dd74d82ec0efb7d1a131ac3e2f809d5a80e2
DylanPrimera <dylanprimera98@gmail.com>
2024-11-13 18:59:58 -0500

    valid categories types

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,37 +1,37 @@
 import { ProductsGrid, Title } from "@/components";
+import { ValidCategory } from "@/interfaces";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: ValidCategory;
   };
 }
 
-const pageCategories = ["men", "women", "kid"];
-
 const burnedProducts = initialData.products;
-
-function capitalizeFirstLetter(text: string) {
-  const capText = text.charAt(0).toUpperCase() + text.slice(1);
-  return capText;
-}
 
 export default function CategoryPage({ params }: Props) {
   const { id } = params;
   const categoryProducts = burnedProducts.filter(
     (product) => product.gender === id
   );
-
-  if (!pageCategories.includes(id!)) {
+  const validCategories: Record<ValidCategory, string> = {
+    'men': "for men",
+    'women': "for women",
+    'kid': "for kid",
+    'unisex': "for everybody"
+  }
+ 
+  if (!(id in validCategories)) {
     notFound();
   }
 
   return (
     <>
       <Title
-        title={capitalizeFirstLetter(id)}
-        subtitle={`All ${id}'s products`}
+        title={`Articles ${validCategories[id]}`}
+        subtitle={`All ${id}s products`}
       />
       <ProductsGrid products={categoryProducts} />
     </>
