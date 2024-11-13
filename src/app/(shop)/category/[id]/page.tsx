b7fbcd22a7b95da6b02786f262e01b42211ee1d5
DylanPrimera <dylanprimera98@gmail.com>
2024-11-13 18:39:58 -0500

    fragment con category page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -10,28 +10,30 @@ interface Props {
 
 const pageCategories = ["men", "women", "kid"];
 
-const products = initialData.products;
+const burnedProducts = initialData.products;
 
 function capitalizeFirstLetter(text: string) {
-  const capText =text.charAt(0).toUpperCase() + text.slice(1);
-  return capText
+  const capText = text.charAt(0).toUpperCase() + text.slice(1);
+  return capText;
 }
 
 export default function CategoryPage({ params }: Props) {
   const { id } = params;
+  const categoryProducts = burnedProducts.filter(
+    (product) => product.gender === id
+  );
 
   if (!pageCategories.includes(id!)) {
     notFound();
   }
-  const categoryProducts = products.filter((product) => product.gender === id);
 
   return (
-    <div>
+    <>
       <Title
         title={capitalizeFirstLetter(id)}
         subtitle={`All ${id}'s products`}
       />
-      <ProductsGrid products={categoryProducts}/>
-    </div>
+      <ProductsGrid products={categoryProducts} />
+    </>
   );
 }
