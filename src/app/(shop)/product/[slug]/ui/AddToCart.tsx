3bb25b5e8454e0986319946ef8e3c2ecd24f4322
DylanPrimera <dylanprimera98@gmail.com>
2024-11-18 11:24:22 -0500

    set quantity and size

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { QuantitySelector, SizeSelector } from "@/components";
 import { Product, ValidSize } from "@/interfaces";
-import React, {  useState } from "react";
+import React, { useState } from "react";
 
 interface Props {
   product: Product;
@@ -9,12 +9,20 @@ interface Props {
 
 export const AddToCart = ({ product }: Props) => {
   const [size, setSize] = useState<ValidSize>();
+  const [quantity, setQuantity] = useState(1);
 
   const handleSizeChange = (size: ValidSize) => {
     if (size) {
       setSize(size);
     }
-  }
+  };
+
+  const handleQuantityChange = (value: number) => {
+    if (quantity + value < 1) return;
+    if (value) {
+      setQuantity(quantity + value);
+    }
+  };
 
   return (
     <>
@@ -24,7 +32,11 @@ export const AddToCart = ({ product }: Props) => {
         onSizeChange={handleSizeChange}
       />
 
-      <QuantitySelector quantity={2} />
+      <QuantitySelector
+        quantity={quantity}
+        onQuantityChange={handleQuantityChange}
+        customClass="mt-5 mb-2"
+      />
 
       <button className="btn-primary my-5">Add to cart</button>
     </>
