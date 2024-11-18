f6b427a43e74c9962fa6be3d50e1ce79c5bb4e31
DylanPrimera <dylanprimera98@gmail.com>
2024-11-18 12:51:52 -0500

    product cart items skeleton minor change

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -2,23 +2,20 @@ import { Title } from "@/components";
 import Link from "next/link";
 import { CartProducts } from "./ui/CartProducts";
 
-
-
 export default function CartPage() {
-
   return (
     <div className="flex justify-center items-center mb-24 sm:mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
         <Title title="Cart" />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 ">
           <div className="flex flex-col mt-5 order-last md:order-first">
-            {/* Cart Items*/}
-            <CartProducts/>
             {/* Some Info */}
             <span className="text-xl">Add more products</span>
             <Link href={"/"} className="underline mb-5">
               Purchase products
             </Link>
+            {/* Cart Items*/}
+            <CartProducts />
           </div>
 
           {/* Checkout*/}
@@ -35,7 +32,12 @@ export default function CartPage() {
               <span className="mt-5 text-2xl text-right">$115</span>
             </div>
             <div className="mt-5 mb-2 w-full">
-              <Link href={"/checkout/address"} className="flex btn-primary justify-center">Checkout</Link>
+              <Link
+                href={"/checkout/address"}
+                className="flex btn-primary justify-center"
+              >
+                Checkout
+              </Link>
             </div>
           </div>
         </div>
--- src/app/(shop)/cart/ui/CartProducts.tsx
+++ src/app/(shop)/cart/ui/CartProducts.tsx
@@ -1,47 +1,67 @@
 "use client";
-import { QuantitySelector } from "@/components";
+import { QuantitySelector, Skeleton } from "@/components";
 import { useCartStore } from "@/store";
 import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const CartProducts = () => {
   const cartProducts = useCartStore((state) => state.cartItems);
-  const updateProducQuantity = useCartStore(
+  const updateProductQuantity = useCartStore(
     (state) => state.updateProductQuantity
   );
-  if (cartProducts.length <= 0) {
+  const [loaded, setLoaded] = useState(false);
+
+  // effect to avoid hydratation issue
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) return <Skeleton customClass="h-full" />;
+
+  if (loaded && cartProducts.length <= 0) {
     redirect("/empty");
   }
-
-  return (
-    <>
-      {cartProducts.map((product, index) => (
-        <div key={index} className="flex items-center mb-5">
-          <Image
-            src={`/products/${product.image}`}
-            alt={product.title}
-            className="mr-5 rounded"
-            width={100}
-            height={100}
-          />
-          <div>
-            <div className="flex items-center gap-1">
-              <p>{product.size}</p>
-              <span>-</span>
-              <p>{product.title}</p>
-            </div>
-
-            <p>$ {product.price.toFixed(2)}</p>
-            <QuantitySelector
-              quantity={product.quantity}
-              onQuantityChange={(quantity) =>
-                updateProducQuantity(product, quantity)
-              }
+  if (loaded) {
+    return (
+      <>
+        {cartProducts.map((product) => (
+          <div
+            key={`${product.slug}-${product.size}`}
+            className="flex items-center mb-5"
+          >
+            <Image
+              src={`/products/${product.image}`}
+              alt={product.title}
+              className="mr-5 rounded"
+              width={100}
+              height={100}
             />
-            <button className="underline mt-3">Remove</button>
+            <div>
+              <div className="flex items-center gap-1">
+                <p>{product.size}</p>
+                <span>-</span>
+                <Link
+                  href={`/product/${product.slug}`}
+                  className="hover:underline hover:text-gray-700 antialiased cursor-pointer"
+                >
+                  {product.title}
+                </Link>
+              </div>
+
+              <p>$ {product.price.toFixed(2)}</p>
+              <QuantitySelector
+                quantity={product.quantity}
+                onQuantityChange={(quantity) =>
+                  updateProductQuantity(product, quantity)
+                }
+              />
+              <button className="underline mt-3">Remove</button>
+            </div>
           </div>
-        </div>
-      ))}
-    </>
-  );
+        ))}
+      </>
+    );
+  }
 };
